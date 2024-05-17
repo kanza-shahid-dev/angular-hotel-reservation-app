36a5aaeb81981e1c3e44f738ecf0f97a7ca7f4a6
Kanza Shahid <kanza.shahid@teo-intl.com>
2024-05-17 12:54:16 +0500

    project architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationFormComponent
   ],
   imports: [
     BrowserModule,
